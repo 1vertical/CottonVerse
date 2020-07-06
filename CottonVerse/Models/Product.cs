39922e5ed29e1a3fa369d21a6dc264e523404d98
Shaun Smith <45996226+1vertical@users.noreply.github.com>
2020-07-06 16:20:21 +0200

    Add files via upload

---
+++ CottonVerse/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CottonVerse.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string MaterialInner { get; set; }
+        public string MaterialOuter { get; set; }
+        public string Color { get; set; }
+        public string Texture { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public int StockOnHand { get; set; }
+        public decimal Price { get; set; }
+        public decimal ProfitMargin { get; set; }
+    }
+}
